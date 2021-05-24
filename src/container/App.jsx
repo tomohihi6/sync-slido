1c68b44e97c4621b07d56fc4062c309e20b93dd4
Tomohiro Kawatani <b1017111@fun.ac.jp>
2021-05-24 16:14:39 +0900

    testing reveal.js slide show

--- src/container/App.jsx
+++ src/container/App.jsx
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Reveal from 'reveal.js';
+import Markdown from 'reveal.js/plugin/markdown/markdown.esm';
+import 'reveal.js/dist/reset.css';
+import 'reveal.js/dist/reveal.css';
+import 'reveal.js/dist/theme/black.css';
 import '../styles/App.css';
 
 function App() {
+  let deck;
+  useEffect(() => {
+    deck = new Reveal({ histroy: true, plugins: [Markdown] });
+    deck.initialize();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <div className="reveal" style={{ height: '90%' }}>
+        <div className="slides">
+          <section>テスト</section>
+          <section>CDDDDD</section>
+          <section>AAAAAA</section>
+        </div>
+      </div>
+      <button
+        onClick={() => {
+          const state = deck.getState();
+          console.log(state);
+        }}>
+        右へ
+      </button>
     </div>
   );
 }
